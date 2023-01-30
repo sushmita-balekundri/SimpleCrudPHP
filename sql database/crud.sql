ae0abd71bbca3fc1102e32f48e458b735be376e9
sushmita09b <sushmitabalekundri@questglt.com>
2023-01-30 12:32:57 +0530

    database

--- sql database/crud.sql
+++ sql database/crud.sql
@@ -3,9 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Apr 02, 2021 at 08:41 AM
+-- Generation Time: Jan 30, 2023 at 08:02 AM
 -- Server version: 10.4.14-MariaDB
--- PHP Version: 7.2.33
+-- PHP Version: 7.3.22
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -40,11 +40,11 @@ CREATE TABLE `student` (
 --
 
 INSERT INTO `student` (`sid`, `sname`, `saddress`, `sclass`, `sphone`) VALUES
-(1, 'Sila Khatri', '#34 sdfdsfsdf', 1, '9898989874'),
-(2, 'Anita Chettri', '#654 KD Building Mumbai', 1, '9874587456'),
+(2, 'Anita Chettri12333', '#654 KD Building Mumbai', 1, '9874587456'),
 (11, 'Jasmine Singh', '#34 JC Road, abc', 1, '9845546546'),
 (12, 'Sangam B', '#76 SK Building,Pkr', 3, '9827135435'),
-(13, 'Ritika Singh', '#45 RS Buiding', 3, '34435435');
+(13, 'Ritika Singh', '#45 RS Buiding', 3, '34435435'),
+(16, 'sdsf', 'sfsdf', 3, 'sdfds');
 
 -- --------------------------------------------------------
 
@@ -91,7 +91,7 @@ ALTER TABLE `studentclass`
 -- AUTO_INCREMENT for table `student`
 --
 ALTER TABLE `student`
-  MODIFY `sid` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
+  MODIFY `sid` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=17;
 
 --
 -- AUTO_INCREMENT for table `studentclass`
